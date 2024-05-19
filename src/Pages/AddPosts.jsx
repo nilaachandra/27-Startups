78dd688683b6069fe649b3f00883711f4729aaa9
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-05-19 13:08:41 +0530

    post add form integrated with supabase

--- src/Pages/AddPosts.jsx
+++ src/Pages/AddPosts.jsx
@@ -2,21 +2,24 @@ import React, { useState } from "react";
 import { RiErrorWarningFill } from "react-icons/ri";
 import { FaAt } from "react-icons/fa";
 import Button from "../Components/Button";
-import { Link } from "react-router-dom";
 import { useSupaContext } from "../contexts/SupaContext";
+import { Toaster, toast } from "sonner";
+import Loader from "../Components/Loader";
+
 const AddPosts = () => {
   const { postIdea, loading, error } = useSupaContext();
   const [description, setDescription] = useState("");
   const [username, setUsername] = useState("");
   const [socialHandle, setSocialHandle] = useState("");
-  const [social, setSocial] = useState("Twitter");
-
+  const [socialType, setSocialType] = useState("Twitter");
+  const [isPosting, setIsPosting] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
     let twitter = false;
     let instagram = false;
     let github = false;
-    switch (social) {
+
+    switch (socialType) {
       case "Twitter":
         twitter = true;
         break;
@@ -30,12 +33,30 @@ const AddPosts = () => {
         break;
     }
 
-    await postIdea(ideaData);
+    // Show loading state in button
+    setIsPosting(true);
+    // Simulate loading with setTimeout for 2 seconds
+    setTimeout(async () => {
+      const ideaData = {
+        description,
+        username,
+        social_username: socialHandle,
+        twitter,
+        instagram,
+        github,
+      };
 
-    setDescription("");
-    setUsername("");
-    setSocialHandle("");
-    setSocial("Twitter");
+      await postIdea(ideaData);
+      // Clear form fields after posting
+      setDescription("");
+      setUsername("");
+      setSocialHandle("");
+      setSocialType("Twitter");
+      // Reset loading state
+      setIsPosting(false);
+      // Show success toast
+      toast.success("Startup Idea posted successfully!🚀");
+    }, 1500);
   };
 
   return (
@@ -43,64 +64,97 @@ const AddPosts = () => {
       <h1 className="text-base lg:text-3xl mb-4 headfont-bold">
         Add Your potential Unicorn Product/Startup Idea!
       </h1>
-      <label htmlFor="" className="flex flex-col gap-2">
-        <span className="lg:text-lg text-sm text-light-text">Your Idea💡</span>
-        <textarea
-          name=""
-          id=""
-          rows={6}
-          className="rounded-md border p-2 border-light-text"
-          placeholder="What was that Idea that popped up in your mind while you were Pooping?"
-        ></textarea>
-      </label>
-      <label htmlFor="" className="flex flex-col gap-2">
-        <div className="flex justify-between items-center">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="description" className="flex flex-col gap-2">
           <span className="lg:text-lg text-sm text-light-text">
-            Your Username👨🏻‍💼
+            Your Idea💡
           </span>
-          <span className="lg:text-lg text-sm text-light-text flex items-center gap-1">
-            <RiErrorWarningFill />
-            No Account Needed!
-          </span>
-        </div>
-        <input
-          type="text"
-          name=""
-          id=""
-          className="rounded-md p-3 border border-light-text"
-        />
-      </label>
-
-      <label htmlFor="" className="flex flex-col w-full gap-2">
-        <span className="lg:text-lg text-sm text-light-text">
-          {" "}
-          Show off Your Socials🤳🏽
-        </span>
-        <div className="flex items-center w-full flex-col lg:gap-0 gap-3 lg:flex-row">
-          <div className="flex items-center w-full">
-            <span className="text-light-text rounded-l-md bg-zinc-400 p-3">
-              <FaAt size={25} />
+          <textarea
+            id="description"
+            rows={6}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="rounded-md border p-2 border-light-text"
+            placeholder="What was that Idea that popped up in your mind while you were Pooping?"
+            required
+          ></textarea>
+        </label>
+        <label htmlFor="username" className="flex flex-col gap-2">
+          <div className="flex justify-between items-center">
+            <span className="lg:text-lg text-sm text-light-text">
+              Your Username👨🏻‍💼
             </span>
-            <input
-              type="text"
-              name=""
-              id=""
-              className="rounded-r-md p-3 w-full border border-zinc-400"
-            />
+            <span className="lg:text-lg text-sm text-light-text flex items-center gap-1">
+              <RiErrorWarningFill />
+              No Account Needed!
+            </span>
           </div>
-          <select className="p-3 rounded-md lg:ml-2 ml-0 border border-zinc-400 w-full lg:w-1/2">
-            <option value="Twitter">Twitter</option>
-            <option value="Twitter">Instagram</option>
-            <option value="Twitter">Github</option>
-          </select>
-        </div>
-      </label>
-      <Button className="group py-3 flex gap-3 items-center text-lg">
-        <span>Post</span>
-        <span className="inline-block group-hover:scale-150 transition-all duration-200">
-          ➤
-        </span>
-      </Button>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="rounded-md p-3 border border-light-text"
+            required
+          />
+        </label>
+
+        <label htmlFor="social" className="flex flex-col w-full gap-2">
+          <span className="lg:text-lg text-sm text-light-text">
+            Show off Your Socials🤳🏽
+          </span>
+          <div className="flex items-center w-full flex-col lg:gap-0 gap-3 lg:flex-row">
+            <div className="flex items-center w-full">
+              <span className="text-light-text rounded-l-md bg-zinc-400 p-3">
+                <FaAt size={25} />
+              </span>
+              <input
+                type="text"
+                id="social"
+                value={socialHandle}
+                onChange={(e) => setSocialHandle(e.target.value)}
+                className="rounded-r-md p-3 w-full border border-zinc-400"
+                placeholder="Your social media handle..."
+              />
+            </div>
+            <select
+              id="socialType"
+              value={socialType}
+              onChange={(e) => setSocialType(e.target.value)}
+              className="p-3 rounded-md lg:ml-2 ml-0 border border-zinc-400 w-full lg:w-1/2"
+            >
+              <option value="Twitter">Twitter</option>
+              <option value="Instagram">Instagram</option>
+              <option value="Github">Github</option>
+            </select>
+          </div>
+        </label>
+
+        <Button
+          type="submit"
+          className="group py-3 w-full mt-4 flex gap-3 items-center justify-center text-lg"
+          disabled={isPosting}
+        >
+          {isPosting ? (
+            <span className="flex items-center gap-3">
+              <Loader
+              type='spin'
+                color="#ffffff"
+                height={25}
+                width={25}
+              />
+              Posting
+            </span>
+          ) : (
+            <span>Post</span>
+          )}
+          <span className="inline-block group-hover:scale-150 transition-all duration-200">
+            ➤
+          </span>
+        </Button>
+      </form>
+      {error && <p className="text-red-500">{error}</p>}
+      <Toaster richColors position="top-center"/>
     </div>
   );
 };
